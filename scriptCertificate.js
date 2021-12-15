67aabea5494ca1cb2423983085eb8ac0755a7366
AntonSheluho <grinfrend2@gmail.com>
2021-12-15 16:19:16 +0300

    adaptiv

--- scriptCertificate.js
+++ scriptCertificate.js
@@ -4,3 +4,16 @@ certificate.style.cursor = 'pointer';
 certificate.onclick = () => certificate.classList.toggle('bigList');
 
 document.addEventListener('scroll', () => certificate.classList.remove('bigList'));
+
+const burger = document.querySelector('.burger');
+const titleNavig = document.querySelector('.title__navig');
+
+burger.onclick = () => {
+    burger.classList.toggle('activeBurger');
+    titleNavig.classList.toggle('activeTitleNavig');
+}
+document.onscroll = () => {
+    burger.classList.remove('activeBurger');
+    titleNavig.classList.remove('activeTitleNavig');
+}
+
